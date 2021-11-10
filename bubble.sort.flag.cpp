39778676ff31a858f945303c7c05c0d04dc1a316
Iman beigy <91473452+Imanbeigy@users.noreply.github.com>
2021-11-10 04:46:11 +0330

    Update bubble.sort.flag.cpp

--- bubble.sort.flag.cpp
+++ bubble.sort.flag.cpp
@@ -4,7 +4,7 @@ int main()
 {
 		int i, j, flag = 1;    // set flag to 1 to start first pass
    		int num[5]={3,4,5,6,2};
-        int numLength = 5; 
+                int numLength = 5; 
       for(i = 0; (i < numLength) && flag; i++)  //if flag becomes 0 then sorting stops
      {
           flag = 0;
